dcc2613a92ad201ba50cb88f27fe0e507dd3f92c
mkaply <mkaply@a30882c0-3a4e-0410-b619-9b4a0ecba449>
2009-03-26 18:24:14 +0000

    build scripts

---
+++ buildit.sh
@@ -0,0 +1,17 @@
+TOOLBARID=activities@kaply.com
+# The shortname of the extension
+SHORTNAME=activities
+rm  *.xpi
+rm -rf $TOOLBARID
+mkdir $TOOLBARID
+cd $TOOLBARID
+rsync -r --exclude=.svn --exclude-from=../excludefile.txt ../* .
+rm chrome.manifest
+rm chrome.manifest.flat
+mv chrome.manifest.jar chrome.manifest
+cd ..
+cd $TOOLBARID
+VERSION=`grep "em:version" install.rdf | sed -e 's/[ \t]*em:version=//;s/"//g'`
+TOOLBAR=bt-$SHORTNAME-$VERSION
+zip -r -D ../$TOOLBAR.xpi *
+cd ..
